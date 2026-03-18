31305ea8c746e9144c3ec204e89648a5a94e95b6
Mason <mirabito.mason@gmail.com>
2026-03-18 16:40:18 +1030

    bear-plugins code standards compliant

--- shortest-column.js
+++ shortest-column.js
@@ -1,10 +1,24 @@
-const style = document.createElement('style');
-style.textContent = `.masonry {
+/*
+ Plugin name: image-masonry
+ Description: A simple hacky masonry grid that pulls photos from a github repository
+ Author: Mason
+ Author URI: mason.bearblog.dev, github.com/IAMME543
+ */
+
+(function () {
+    'use strict';
+    const main = document.querySelector('main');
+
+    if (!main || !document.body.classList.contains('blog')) { return }
+
+    const style = document.createElement('style');
+
+    style.textContent = `
+    .masonry {
   display: flex;
   flex-direction: row;
   gap: 10px;
   }
-
   .masonry img {
     width: 100%;
     height: auto;
@@ -23,68 +37,90 @@ style.textContent = `.masonry {
     flex-direction: column;
     gap: 10px;
 }`;
-document.head.appendChild(style);
 
-const script = document.currentScript;
+    document.head.appendChild(style);
 
-const owner = script.dataset.owner;
-const repo = script.dataset.repo;
-const folder = script.dataset.folder;
+    const script = document.currentScript;
 
-const columncount = Number(script.dataset.columns);
-const columns = [];
+    const owner = script.dataset.owner;
+    const repo = script.dataset.repo;
+    const folder = script.dataset.folder;
+    const columnCount = Number(script.dataset.columns);
 
-let columnHeight = new Array(columncount).fill(1);
+    const columns = [];
 
-const main = document.querySelector('main');
-
-const container = document.createElement('div');
-container.classList.add('masonry');
-main.appendChild(container);
+    let columnHeight = new Array(columnCount).fill(1);
 
 
-Array.from({ length: columncount }).forEach(col => {
-    const column = document.createElement('div');
-    column.classList.add('column');
-    columns.push(column);
-    container.appendChild(column);
-});
+    const container = document.createElement('div');
+
+    container.classList.add('masonry');
+
+    main.appendChild(container);
 
 
-async function fetchImages() {
-    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${folder}`;
+    Array.from({ length: columnCount }).forEach(col => {
 
-    try {
-        const res = await fetch(apiUrl);
-        const data = await res.json();
+        const column = document.createElement('div');
+
+        column.classList.add('column');
+
+        columns.push(column);
+
+        container.appendChild(column);
+
+    });
 
 
-        // filter for image files
-        const urls = data
-            .filter(file => file.type === 'file' && /\.(jpg|jpeg|png|webp)$/i.test(file.name))
-            .map(file => file.download_url);
+    async function fetchImages() {
 
-        urls.forEach(url => {
-            const img = document.createElement('img');
-            img.addEventListener('load', () => {
-                let shortest = columnHeight.indexOf(Math.min(...columnHeight));
+        const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${folder}`;
 
-                //console.log(columnHeight[0] + ", " + columnHeight[1] + ", " + columnHeight[2])
+        try {
 
-                columns[shortest].appendChild(img);
+            const res = await fetch(apiUrl);
+            const data = await res.json();
 
-                columnHeight[shortest] += img.getBoundingClientRect().height;
-                //console.log("url: " + url + " shortest: " + shortest + " height: " + columnHeight[shortest])
 
-                img.classList.add('loaded');
-            });
-            img.src = url;
-            img.alt = url;
-        })
-    } catch (err) {
-        console.error("Error fetching images:", err);
-        container.innerHTML = "<p>Failed to load images.</p>";
+            // filter for image files
+
+            const urls = data
+                .filter(file => file.type === 'file' && /\.(jpg|jpeg|png|webp)$/i.test(file.name))
+                .map(file => file.download_url);
+
+
+
+            urls.forEach(url => {
+                const img = document.createElement('img');
+                img.addEventListener('load', () => {
+
+                    let shortest = columnHeight.indexOf(Math.min(...columnHeight));
+
+                    //console.log(columnHeight[0] + ", " + columnHeight[1] + ", " + columnHeight[2])
+
+                    columns[shortest].appendChild(img);
+                    columnHeight[shortest] += img.getBoundingClientRect().height;
+
+                    //console.log("url: " + url + " shortest: " + shortest + " height: " + columnHeight[shortest])
+
+                    img.classList.add('loaded');
+
+                });
+                img.src = url;
+                img.alt = url;
+            })
+
+        } catch (err) {
+
+            console.error("Error fetching images:", err);
+            container.innerHTML = "<p>Failed to load images.</p>";
+
+        }
+
     }
-}
 
-fetchImages();
+
+    addEventListener("DOMContentLoaded", (event) => fetchImages())
+
+
+})();

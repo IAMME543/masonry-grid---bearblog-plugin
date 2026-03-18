474c47817b08edd7a8809c128fb187d9040b4f4c
Mason <mirabito.mason@gmail.com>
2026-03-18 16:19:05 +1030

    WE ARE BACK BABY! Copied how masonry is meant to be done and waited onload to get image heights for shortest-column.js

---
+++ shortest-column.js
@@ -0,0 +1,90 @@
+const style = document.createElement('style');
+style.textContent = `.masonry {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  }
+
+  .masonry img {
+    width: 100%;
+    height: auto;
+    object-fit: contain;
+    display: block;
+    transition: opacity 0.3s ease-in-out;
+    opacity: 0;
+  }
+
+  .masonry img.loaded {
+    opacity: 1;
+  }
+
+  .column {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}`;
+document.head.appendChild(style);
+
+const script = document.currentScript;
+
+const owner = script.dataset.owner;
+const repo = script.dataset.repo;
+const folder = script.dataset.folder;
+
+const columncount = Number(script.dataset.columns);
+const columns = [];
+
+let columnHeight = new Array(columncount).fill(1);
+
+const main = document.querySelector('main');
+
+const container = document.createElement('div');
+container.classList.add('masonry');
+main.appendChild(container);
+
+
+Array.from({ length: columncount }).forEach(col => {
+    const column = document.createElement('div');
+    column.classList.add('column');
+    columns.push(column);
+    container.appendChild(column);
+});
+
+
+async function fetchImages() {
+    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${folder}`;
+
+    try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+
+
+        // filter for image files
+        const urls = data
+            .filter(file => file.type === 'file' && /\.(jpg|jpeg|png|webp)$/i.test(file.name))
+            .map(file => file.download_url);
+
+        urls.forEach(url => {
+            const img = document.createElement('img');
+            img.addEventListener('load', () => {
+                let shortest = columnHeight.indexOf(Math.min(...columnHeight));
+
+                //console.log(columnHeight[0] + ", " + columnHeight[1] + ", " + columnHeight[2])
+
+                columns[shortest].appendChild(img);
+
+                columnHeight[shortest] += img.getBoundingClientRect().height;
+                //console.log("url: " + url + " shortest: " + shortest + " height: " + columnHeight[shortest])
+
+                img.classList.add('loaded');
+            });
+            img.src = url;
+            img.alt = url;
+        })
+    } catch (err) {
+        console.error("Error fetching images:", err);
+        container.innerHTML = "<p>Failed to load images.</p>";
+    }
+}
+
+fetchImages();
